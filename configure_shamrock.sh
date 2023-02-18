1a414f0b87499f5695c6e4ea4f13ad133f2a45d3
tdavidcl <timothee.davidcleri@gmail.com>
2023-02-18 13:54:41 +0100

    configure_shamrock

---
+++ configure_shamrock.sh
@@ -0,0 +1,42 @@
+Buildtool="ninja"
+if ! type ninja > /dev/null; then
+    Buildtool="make"
+fi
+
+echo "current build system : ${Buildtool}"
+
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/dpcpp_cuda_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/dpcpp_cuda_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/dpcpp_cudai32_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/dpcpp_cudai32_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/opensycl_omp_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_omp_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_omp_asan_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp_sanitizer
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/opensycl_generic_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_generic_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
+
+
+
+
+
+
+
+ln -sf Shamrock/build_config/dpcpp_cuda_release Shamrock/build
--- quickstart.sh
+++ quickstart.sh
@@ -1,23 +1,11 @@
+git submodule init
 git submodule update
 
 # setup the compilers
 sh setup_compilers.sh
 
 # clone shamrock
-git clone git@github.com:tdavidcl/Shamrock.git
+git clone --recurse-submodules git@github.com:tdavidcl/Shamrock.git
 
 source exports.sh
 
-cd Shamrock
-
-
-
-Buildtool="ninja"
-if ! type ninja > /dev/null; then
-    Buildtool="make"
-fi
-
-echo "current build system : ${Buildtool}"
-
-
-# run shamrock buildbot
