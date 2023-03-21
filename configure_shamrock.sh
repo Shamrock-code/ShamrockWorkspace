7e4a63173380d826866a64abb4a6fb3c8c68f768
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-03-21 21:36:24 +0100

    fix

--- configure_shamrock.sh
+++ configure_shamrock.sh
@@ -5,42 +5,39 @@ fi
 
 echo "current build system : ${Buildtool}"
 
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/dpcpp_cuda_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/dpcpp_cuda_profiling --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-profiling
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/dpcpp_cuda_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/dpcpp_cudai32_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/dpcpp_cudai32_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/opensycl_omp_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_omp_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_omp_asan_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp_sanitizer
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build release \
+    --outdir Shamrock/build_config/opensycl_generic_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
+
+python3 Shamrock/buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
+    --outdir Shamrock/build_config/opensycl_generic_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
+
+
+
 cd Shamrock
-
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build release \
-    --outdir build_config/dpcpp_cuda_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build release \
-    --outdir build_config/dpcpp_cuda_profiling --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-profiling
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
-    --outdir build_config/dpcpp_cuda_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build release \
-    --outdir build_config/dpcpp_cudai32_release --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
-    --outdir build_config/dpcpp_cudai32_debug --cxxpath sycl_compilers/dpcpp --compiler dpcpp --profile cuda-index32bit
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build release \
-    --outdir build_config/opensycl_omp_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
-    --outdir build_config/opensycl_omp_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
-    --outdir build_config/opensycl_omp_asan_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile omp_sanitizer
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build release \
-    --outdir build_config/opensycl_generic_release --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
-
-python3 buildbot/configure.py --gen "${Buildtool}" --tests --build debug \
-    --outdir build_config/opensycl_generic_debug --cxxpath sycl_compilers/opensycl --compiler opensycl --profile generic
-
-
-
-
 
 
 ln -sf build_config/dpcpp_cuda_release build
