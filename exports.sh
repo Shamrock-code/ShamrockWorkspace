b49c6c3cd622e3f8c1afc3888b2d38848494ec80
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-11-21 13:40:03 +0100

    add activate script

--- exports.sh
+++
@@ -1,7 +0,0 @@
-export ILLVM_HOME=$(pwd)/sycl_compilers/intel_llvm/
-export PATH=$ILLVM_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$ILLVM_HOME/lib:$LD_LIBRARY_PATH
-
-export ACPP_HOME=$(pwd)/sycl_compilers/acpp/
-export PATH=$ACPP_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$ACPP_HOME/lib:$LD_LIBRARY_PATH
